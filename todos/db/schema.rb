30b78b21b75fa409eb0b6f3597c50db32b455960
André Pinho <andresouzapinho@gmail.com>
2020-04-27 21:40:44 -0300

    Add budget to project to ensure croods can handle floats

--- todos/db/schema.rb
+++ todos/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_193027) do
+ActiveRecord::Schema.define(version: 2020_04_28_002955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_193027) do
     t.datetime "created_at",  :null=>false
     t.datetime "updated_at",  :null=>false
     t.boolean  "highlighted", :default=>false, :null=>false
+    t.float    "budget"
   end
 
   create_table "lists", force: :cascade do |t|
